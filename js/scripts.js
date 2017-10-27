07a38206de14f43d560858164f367e647149a6bc
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-10-26 21:05:38 -0700

    totaly refactor the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,108 +1,20 @@
-var romanNumeral = function (number) {
-  var romanNumeral = "";
-  var remainder = number;
+var romanNumeral = function (userNumber) {
+  var thousands = ["","M","MM","MMM"];
+  var hundreds = ["","C","CC", "CCC", "CD", "D","DC","DCC","DCCC","CM"];
+  var tens = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+  var singles = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
 
-  if (number > 4000) {
-    alert("This number is too large! Only numbers less than 4,000 may be converted to Roman Numerals");
-  } else if (isNaN(number)) {
-    alert("Please enter a number");
-  } else {
-    if (remainder >= 1000) {
-      remainder = number % 1000;
-      number = number / 1000;
-      number = Math.floor(number);
-      for (index = 1; index <= number; index++) {
-        romanNumeral = romanNumeral.concat("M");
-      }
-    }
-    if (remainder >= 500) {
-      number = remainder / 500;
-      number = Math.floor(number);
-      //console.log(number);
-      remainder = remainder % 500;
-      //console.log(remainder);
-      if (remainder >= 400) {
-        romanNumeral = romanNumeral.concat("CM");
-        remainder = remainder % 400;
-        //console.log(remainder);
-      } else {
-      for (index = 1; index <= number; index++) {
-        romanNumeral = romanNumeral.concat("D");
-        }
-      }
-    }
-    if (remainder >= 100) {
-      number = remainder / 100;
-      number = Math.floor(number);
-      //console.log(number);
-      remainder = remainder % 100;
-      //console.log(remainder);
-      if (number === 4) {
-        romanNumeral = romanNumeral.concat("CD")
-      } else {
-      for (index = 1; index <= number; index++) {
-        romanNumeral = romanNumeral.concat("C");
-        }
-      }
-    }
-    if (remainder >= 50) {
-      number = remainder / 50;
-      number = Math.floor(number);
-      //console.log(number);
-      remainder = remainder % 50;
-      //console.log(remainder);
-      if (remainder >= 40) {
-        romanNumeral = romanNumeral.concat("XC");
-        remainder = remainder % 40;
-        //console.log(remainder);
-      } else {
-      for (index = 1; index <= number; index++) {
-        romanNumeral = romanNumeral.concat("L");
-        }
-      }
-    }
-    if (remainder >= 10) {
-      number = remainder / 10;
-      number = Math.floor(number);
-      //console.log(number);
-      remainder = remainder % 10;
-      //console.log(remainder);
-      if (number === 4) {
-        romanNumeral = romanNumeral.concat("XL")
-      } else {
-      for (index = 1; index <= number; index++) {
-        romanNumeral = romanNumeral.concat("X");
-        }
-      }
-    }
-    if (remainder >= 5) {
-      number = remainder / 5;
-      number = Math.floor(number);
-      //console.log(number);
-      remainder = remainder % 5;
-      //console.log(remainder);
-      if (remainder >= 4) {
-        romanNumeral = romanNumeral.concat("IX");
-        remainder = remainder % 4;
-      } else {
-      for (index = 1; index <= number; index++) {
-        romanNumeral = romanNumeral.concat("V");
-        }
-      }
-    }
-    if (remainder > 0) {
-      number = remainder / 1;
-      if (number === 4) {
-        romanNumeral = romanNumeral.concat("IV")
-      } else {
-      for (index = 1; index <= remainder; index++) {
-        romanNumeral = romanNumeral.concat("I");
-        }
-      }
-    }
-  }
-  return romanNumeral;
+  // userNumber/1000 = how many thousands
+  // (userNumber % 1000)/100 = how many hundreds
+  // (userNumber % 100)/10 = how many tens
+  // (userNumber % 10) = how many singles
+
+  return thousands[Math.floor(userNumber/1000)] +
+         hundreds[Math.floor((userNumber % 1000)/100)] +
+         tens[Math.floor((userNumber % 100)/10)] +
+         singles[Math.floor(userNumber % 10)];
 }
+
 
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
